0619243b74c364c04cd2e496744c9687b050275d
patrick rasmussen <mcman407@gmail.com>
2013-07-18 22:26:27 -0400

    new art peice

--- Assets/Scripts/Game Logic/FilmReelScript.cs
+++ Assets/Scripts/Game Logic/FilmReelScript.cs
@@ -1,26 +1,63 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class FilmReelScript : MonoBehaviour {
-	///public GameObject prefab;
-	//public Transform target;
+	public static FilmReelScript instance;
+	public Transform player;
+    public Transform filmReel;
+    public Transform levelObejct;
+	private Transform filmReels;
+    public int numOfobjects;
+    public float recycleOffset;
+  	public float minGap, maxGap, filmReelCount;
+    private Vector3 nextPosition;
+    private Queue<Transform> objectQueue;
+   
 	
-	private int maxCount = 5;
-	private int currentCount = 1;
-	private float speed = 3.0f;
-	void Start () {
-		 
+	
+	void Start(){
+		minGap = 20;
+		maxGap = 60;
+		
 	}
-	
+    // Use this for initialization
+    void Awake()
+    {
+		instance = this;
+        // creates a new queue big as num of objects
+        objectQueue = new Queue<Transform>(numOfobjects);
+        // fills up the queue with the prefabs.
+        for (int i = 0; i < numOfobjects; i++)
+        {
+            objectQueue.Enqueue((Transform)Instantiate(filmReel));
+        }
+        
+         nextPosition = new Vector3(Random.Range(-7,7),Random.Range(.02f,.80f), Random.Range(-20,-10));
+		for (int i = 0; i < numOfobjects; i++)
+        {
+            Recycle();
+        }
+    }
 
-	void Update () {
-	for(int i =0; i <= maxCount; i++){
-			Vector3 pos = new Vector3(this.transform.position.x , this.transform.position.y, this.transform.position.z+3); 
-			Instantiate(this.gameObject,pos,Quaternion.identity);
-			currentCount++;}
-		float amtToMove = speed * Time.deltaTime;
-		this.transform.Translate(Vector3.back * amtToMove, Space.World);
-	
-	
-	}
+    // Update is called once per frame
+    public void Update()
+    {
+		if (objectQueue.Peek().localPosition.z + recycleOffset < player.position.z)
+        {
+            Recycle();
+        }
+		
+    }
+
+    private void Recycle()
+    {	float offset = Random.Range(minGap, maxGap);
+		
+        filmReels = objectQueue.Dequeue();
+        filmReels.parent = levelObejct;
+        filmReels.localPosition = new Vector3(Random.Range(-7,7),Random.Range(.02f,.80f), nextPosition.z);
+        nextPosition.z += offset;
+        
+        objectQueue.Enqueue(filmReels);
+		
+    }
 }
--- Assets/Scripts/Game Logic/Generators/LevelGeneration.cs
+++ Assets/Scripts/Game Logic/Generators/LevelGeneration.cs
@@ -1,45 +1,48 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class LevelGeneration : MonoBehaviour {
-	public Transform prefab;
-	public Transform thisprefab;
-	public int numOfobjects;
-	public float recycleOffset;
-	public float distanceCheck;
-	private Vector3 nextPosition;
-	private Queue<Transform> objectQueue;
-	private int i;
-	// Use this for initialization
-	void Awake() {
-		i = 0;
-		// creates a new queue big as num of objects
-		objectQueue = new Queue<Transform>(numOfobjects);
-		// fills up the queue with the prefabs.
-		//for(int i = 0; i < numOfobjects; i++){
-		//	objectQueue.Enqueue((Transform)Instantiate(prefab));
-		//}
-		nextPosition.z = transform.localPosition.z * .5f ;
-		//for(int i = 0; i < numOfobjects; i++){
-		//	Recycle();
-		//}
-	}
-	
-	// Update is called once per frame
-	public void Update () {
-		if(i < numOfobjects){
-			objectQueue.Enqueue((Transform)Instantiate(prefab));
-			i++;
-		}
-		if(objectQueue.Peek().localPosition.z  < -5){
-			Recycle ();
-		}
-	}
-	private void Recycle(){
-		Transform road = objectQueue.Dequeue();
-		road.parent = thisprefab;
-		road.localPosition += nextPosition ;
-		nextPosition.z += road.localScale.z * .5f ;		
-		objectQueue.Enqueue(road);
-	}
+public class LevelGeneration : MonoBehaviour
+{
+    public Transform prefab;
+    public Transform thisprefab;
+    public int numOfobjects;
+    public float recycleOffset;
+    public float distanceCheck;
+    private Vector3 nextPosition;
+    private Queue<Transform> objectQueue;
+    
+    // Use this for initialization
+    void Awake()
+    {
+        // creates a new queue big as num of objects
+        objectQueue = new Queue<Transform>(numOfobjects);
+        // fills up the queue with the prefabs.
+        for (int i = 0; i < numOfobjects; i++)
+        {
+            objectQueue.Enqueue((Transform)Instantiate(prefab));
+        }
+        nextPosition = transform.localPosition;
+        for (int i = 0; i < numOfobjects; i++)
+        {
+            Recycle();
+        }
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (objectQueue.Peek().localPosition.z + recycleOffset < PlayerPhysics.distTraveled)
+        {
+            Recycle();
+        }
+    }
+    private void Recycle()
+    {
+        Transform road = objectQueue.Dequeue();
+        road.parent = thisprefab;
+        road.localPosition = nextPosition;
+        nextPosition.z += road.localScale.z;
+        
+        objectQueue.Enqueue(road);
+    }
 }
